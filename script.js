960ebd78d5d25a11034d40fc143363d26bb17299
sandeeptottadi <76050316+sandeeptottadi@users.noreply.github.com>
2023-03-19 21:27:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function path() {
+    window.location.href = "https://sandeeptottadi.github.io/Pathfinding-Visualizer";
+}
+
+function sorting() {
+    window.location.href = "https://sandeeptottadi.github.io/Sorting-Visualizer";
+}
